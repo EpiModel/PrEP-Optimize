19db13b47265a23ca4f9cf70aeaab1726a236bf6
Samuel Jenness <smjenness@gmail.com>
2019-09-04 12:36:58 -0400

    Update burnin files

--- test/test.burn.R
+++ test/test.burn.R
@@ -9,24 +9,30 @@ est <- readRDS("est/netest.rda")
 
 param <- param_msm(netstats = netstats,
                    epistats = epistats,
-
-                   hiv.test.rate = c(0.01325, 0.0125, 0.0124),
+                   hiv.test.rate = c(0.00385, 0.00380, 0.00690),
                    hiv.test.late.prob = c(0, 0, 0),
-                   tt.part.supp = c(0.20, 0.20, 0.20),
-                   tt.full.supp = c(0.40, 0.40, 0.40),
-                   tt.dur.supp = c(0.40, 0.40, 0.40),
-                   tx.init.prob = c(0.092, 0.092, 0.127),
-                   tx.halt.part.prob = c(0.0102, 0.0102, 0.0071),
-                   tx.halt.full.rr = 0.9,
-                   tx.halt.dur.rr = 0.5,
-                   tx.reinit.part.prob = c(0.00066, 0.00066, 0.00291),
-                   tx.reinit.full.rr = 2.0,
-                   tx.reinit.dur.rr = 5.0,
-                   aids.mr = 1/104,
-
-                   trans.scale = 1.10,
+                   tx.init.prob = c(0.1775, 0.190, 0.2521),
+                   tt.part.supp = c(0, 0, 0),
+                   tt.full.supp = c(1, 1, 1),
+                   tt.dur.supp = c(0, 0, 0),
+                   tx.halt.part.prob = c(0.0062, 0.0055, 0.0031),
+                   tx.halt.full.rr = c(0.45, 0.45, 0.45),
+                   tx.halt.dur.rr = c(0.45, 0.45, 0.45),
+                   tx.reinit.part.prob = c(0.00255, 0.00255, 0.00255),
+                   tx.reinit.full.rr = c(1, 1, 1),
+                   tx.reinit.dur.rr = c(1, 1, 1),
+                   max.time.off.tx.full.int = 52 * 15,
+                   max.time.on.tx.part.int = 52 * 10,
+                   max.time.off.tx.part.int = 52 * 10,
+                   aids.mr = 1/250,
+                   trans.scale = c(2.21, 0.405, 0.255),
                    acts.scale = 1.00,
-                   acts.aids.vl = 5.75)
+                   acts.aids.vl = 5.75,
+                   prep.start = (52*60) + 1,
+                   riskh.start = 52*59,
+                   prep.start.prob = 0.66,
+                   prep.require.lnt = TRUE,
+                   prep.risk.reassess.method = "year")
 init <- init_msm(prev.ugc = 0,
                  prev.rct = 0,
                  prev.rgc = 0,
@@ -39,9 +45,6 @@ control <- control_msm(simno = 1,
                        save.clin.hist = FALSE)
 
 sim <- netsim(est, param, init, control)
-
-load("scripts/burnin/data/sim.n1007.rda")
-sim$param$acts.scale
 
 df <- as.data.frame(sim, out = "mean")
 names(df)
@@ -94,14 +97,14 @@ mean(tail(df$R0.mean.cs, 52))
 
 # Testing/Timing ------------------------------------------------------
 
-m <- microbenchmark::microbenchmark(f(dat, at = 2))
+m <- microbenchmark::microbenchmark(simnet_msm(dat, at = 2))
 print(m, unit = "ms")
 
 profvis::profvis(f(dat, at = 2), interval = 0.005)
 
 dat <- initialize_msm(est, param, init, control, s = 1)
 
-for (at in 2:200) {
+for (at in 2:52) {
   dat <- aging_msm(dat, at) # 1 ms
   dat <- departure_msm(dat, at) # 10 ms
   dat <- arrival_msm(dat, at) # 7 ms
