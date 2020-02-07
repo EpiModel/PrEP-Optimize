605fdb2b31ba3d3d0e4e24027976a0892bc915a5
smjenness <smjenness@gmail.com>
2020-02-07 17:14:17 -0500

    Update PrEPOptim-BuildDatasets.R

--- analysis/PrEPOptim-BuildDatasets.R
+++ analysis/PrEPOptim-BuildDatasets.R
@@ -8,133 +8,207 @@ library("foreach")
 
 # Reference Scenario ------------------------------------------------------
 
-load("intervention/data/sim.n1000.rda")
+load("intervention/data/sim.n500.rda")
 
+sim <- truncate_sim(sim, at = 261)
 df <- as.data.frame(sim, out = "mean")
-dfr <- select(df, incid, s.num, ir100, prepCurr, prepCurr.hadr, prep.rand.stop)
-dfr <- dfr[-1, ]
+df <- df[-nrow(df), ]
+dim(df)
+head(df)
+tail(df)
+dfr <- df
 head(dfr)
 
 years <- rep(1:10, each = 52)
 dfr$years <- years
+head(dfr)
 
 dfr <- group_by(dfr, years) %>%
   summarise(incid = sum(incid),
-            s.num = sum(s.num)/52,
-            prepCurr = sum(prepCurr)/52,
-            prepCurrHA = sum(prepCurr.hadr)/52,
-            prepRandSt = sum(prep.rand.stop)) %>%
+            prepCurr = mean(prepCurr),
+            OptimInitStarts = sum(OptimInitStarts),
+            OptimInitPrev = mean(OptimInitPrev),
+            PrEPStarts = sum(PrEPStarts),
+            PrEPStartsOptim = sum(PrEPStartsOptim),
+            OptimAdhrStarts = sum(OptimAdhrStarts),
+            OptimAdhrPrev = mean(OptimAdhrPrev),
+            PrEPHighAdr = mean(PrEPHighAdr),
+            OptimRetnStarts = sum(OptimRetnStarts),
+            OptimRetnPrev = mean(OptimRetnPrev),
+            PrEPStopsInd = sum(PrEPStopsInd),
+            PrEPStopsRand = sum(PrEPStopsRand),
+            PrEPStopsRandOptim = sum(PrEPStopsRandOptim)) %>%
   as.data.frame()
-dfr$PSP <- sim$param$prep.start.prob
-dfr$PHA <- sim$param$prep.adhr.dist[3]
-dfr$PDR <- sim$param$prep.discont.rate
-dfr$inf.avert <- NA
-dfr$scenario <- "n1000"
-dfr
+dfr$POIP <- sim$param$prep.optim.init.prob
+dfr$PSPO <- sim$param$prep.start.prob.optim
+dfr$POAC <- sim$param$prep.optim.adhr.cap
+dfr$PADO <- sim$param$prep.adhr.dist.optim[3]
+dfr$PORC <- sim$param$prep.optim.retn.cap
+dfr$PDRO <- sim$param$prep.discont.rate.optim
+dfr$infAvert <- NA
+dfr$scenario <- 500
+dfr <- select(dfr, scenario, everything())
 
-dfr.tot <- data.frame(incid = sum(dfr$incid), s.num = sum(dfr$s.num),
-                      prepCurr = sum(dfr$prepCurr), prepCurrHA = sum(dfr$prepCurrHA),
-                      prepRandSt = sum(dfr$prepRandSt),
-                      PSP = dfr$PSP[1], PHA = dfr$PHA[1], PDR = dfr$PDR[1],
-                      inf.avert = NA, scenario = dfr$scenario[1])
-dfr.tot
+# dfr_tot <- data.frame(scenario = dfr$scenario[1],
+#                       incid = sum(dfr$incid),
+#                       prepCurr = mean(dfr$prepCurr),
+#                       OptimInitStarts = sum(dfr$OptimInitStarts),
+#                       OptimInitPrev = mean(dfr$OptimInitPrev),
+#                       PrEPStarts = sum(dfr$PrEPStarts),
+#                       PrEPStartsOptim = sum(dfr$PrEPStartsOptim),
+#                       OptimAdhrStarts = sum(dfr$OptimAdhrStarts),
+#                       OptimAdhrPrev = mean(dfr$OptimAdhrPrev),
+#                       PrEPHighAdr = mean(dfr$PrEPHighAdr),
+#                       OptimRetnStarts = sum(dfr$OptimRetnStarts),
+#                       OptimRetnPrev = mean(dfr$OptimRetnPrev),
+#                       PrEPStopsInd = sum(dfr$PrEPStopsInd),
+#                       PrEPStopsRand = sum(dfr$PrEPStopsRand),
+#                       PrEPStopsRandOptim = sum(dfr$PrEPStopsRandOptim),
+#                       POIP = dfr$POIP[1],
+#                       PSPO = dfr$PSPO[1],
+#                       POAC = dfr$POAC[1],
+#                       PADO = dfr$PADO[1],
+#                       PORC = dfr$PORC[1],
+#                       PDRO = dfr$PDRO[1],
+#                       infAvert = NA)
+# dfr_tot
 
 
 
 # Counterfactual Scenarios ------------------------------------------------
 
 fn <- list.files("intervention/data/", pattern = "sim", full.names = TRUE)
-fn <- fn[2]
+fn <- fn[8]
+
+load(fn)
+plot(sim, y = c("OptimAdhrStarts", "OptimRetnStarts"))
 
 counter_df_byyear <- function(fn, dfr) {
   load(fn)
-  df <- as.data.frame(sim, out = "mean")
-  dfc <- select(df, incid, s.num, ir100, prepCurr, prepCurr.hadr, prep.rand.stop)
-  dfc <- dfc[-1, ]
+  sim <- truncate_sim(sim, at = 261)
+  dfc <- as.data.frame(sim, out = "mean")
+  dfc <- dfc[-nrow(dfc), ]
   dfc$years <- rep(1:10, each = 52)
   df2 <- group_by(dfc, years) %>%
     summarise(incid = sum(incid),
-              s.num = sum(s.num)/52,
-              prepCurr = sum(prepCurr)/52,
-              prepCurrHA = sum(prepCurr.hadr)/52,
-              prepRandSt = sum(prep.rand.stop)) %>%
+              prepCurr = mean(prepCurr),
+              OptimInitStarts = sum(OptimInitStarts),
+              OptimInitPrev = mean(OptimInitPrev),
+              PrEPStarts = sum(PrEPStarts),
+              PrEPStartsOptim = sum(PrEPStartsOptim),
+              OptimAdhrStarts = sum(OptimAdhrStarts),
+              OptimAdhrPrev = mean(OptimAdhrPrev),
+              PrEPHighAdr = mean(PrEPHighAdr),
+              OptimRetnStarts = sum(OptimRetnStarts),
+              OptimRetnPrev = mean(OptimRetnPrev),
+              PrEPStopsInd = sum(PrEPStopsInd),
+              PrEPStopsRand = sum(PrEPStopsRand),
+              PrEPStopsRandOptim = sum(PrEPStopsRandOptim)) %>%
     as.data.frame()
-  df2$PSP <- sim$param$prep.start.prob
-  df2$PHA <- sim$param$prep.adhr.dist[3]
-  df2$PDR <- sim$param$prep.discont.rate
+  df2$POIP <- sim$param$prep.optim.init.prob
+  df2$PSPO <- sim$param$prep.start.prob.optim
+  df2$POAC <- sim$param$prep.optim.adhr.cap
+  df2$PADO <- sim$param$prep.adhr.dist.optim[3]
+  df2$PORC <- sim$param$prep.optim.retn.cap
+  df2$PDRO <- sim$param$prep.discont.rate.optim
   df2$infAvert <- dfr$incid - df2$incid
   df2$scenario <- strsplit(fn, "[.]")[[1]][2]
+  df2 <- select(df2, scenario, everything())
   return(df2)
 }
 counter_df_byyear(fn, dfr)
 
-counter_df_overall <- function(fn, dfr.tot) {
-  load(fn)
-  df <- as.data.frame(sim, out = "mean")
-  dfc <- select(df, incid, s.num, ir100, prepCurr, prepCurr.hadr, prep.rand.stop)
-  dfc <- dfc[-1, ]
-  df2 <- data.frame(incid = sum(dfc$incid), s.num = sum(dfc$s.num)/52,
-                    prepCurr = sum(dfc$prepCurr)/52, prepCurrHA = sum(dfc$prepCurr.hadr)/52,
-                    prepRandSt = sum(dfc$prep.rand.stop))
-  df2$PSP <- sim$param$prep.start.prob
-  df2$PHA <- sim$param$prep.adhr.dist[3]
-  df2$PDR <- sim$param$prep.discont.rate
-  df2$infAvert <- dfr.tot$incid - df2$incid
-  df2$scenario <- strsplit(fn, "[.]")[[1]][2]
-  return(df2)
-}
-counter_df_overall(fn, dfr.tot)
+# counter_df_overall <- function(fn, dfr.tot) {
+#   load(fn)
+#   df <- as.data.frame(sim, out = "mean")
+#   dfc <- select(df, incid, s.num, ir100, prepCurr, prepCurr.hadr, prep.rand.stop)
+#   dfc <- dfc[-1, ]
+#   df2 <- data.frame(incid = sum(dfc$incid), s.num = sum(dfc$s.num)/52,
+#                     prepCurr = sum(dfc$prepCurr)/52, prepCurrHA = sum(dfc$prepCurr.hadr)/52,
+#                     prepRandSt = sum(dfc$prep.rand.stop))
+#   df2$PSP <- sim$param$prep.start.prob
+#   df2$PHA <- sim$param$prep.adhr.dist[3]
+#   df2$PDR <- sim$param$prep.discont.rate
+#   df2$infAvert <- dfr.tot$incid - df2$incid
+#   df2$scenario <- strsplit(fn, "[.]")[[1]][2]
+#   return(df2)
+# }
+# counter_df_overall(fn, dfr.tot)
 
 
 
 # Scale Up on Hyak --------------------------------------------------------
 
-load("data/hold/sim.n1000.rda")
+library("EpiModelHIV")
+library("dplyr")
+library("foreach")
 
+load("data/sim.n500.rda")
+
+sim <- truncate_sim(sim, at = 261)
 df <- as.data.frame(sim, out = "mean")
-dfr <- select(df, incid, s.num, ir100, prepCurr, prepCurr.hadr, prep.rand.stop)
-dfr <- dfr[-1, ]
+df <- df[-nrow(df), ]
+dim(df)
+head(df)
+tail(df)
+dfr <- df
 head(dfr)
 
 years <- rep(1:10, each = 52)
 dfr$years <- years
+head(dfr)
 
 dfr <- group_by(dfr, years) %>%
   summarise(incid = sum(incid),
-            s.num = sum(s.num)/52,
-            prepCurr = sum(prepCurr)/52,
-            prepCurrHA = sum(prepCurr.hadr)/52,
-            prepRandSt = sum(prep.rand.stop)) %>%
+            prepCurr = mean(prepCurr),
+            OptimInitStarts = sum(OptimInitStarts),
+            OptimInitPrev = mean(OptimInitPrev),
+            PrEPStarts = sum(PrEPStarts),
+            PrEPStartsOptim = sum(PrEPStartsOptim),
+            OptimAdhrStarts = sum(OptimAdhrStarts),
+            OptimAdhrPrev = mean(OptimAdhrPrev),
+            PrEPHighAdr = mean(PrEPHighAdr),
+            OptimRetnStarts = sum(OptimRetnStarts),
+            OptimRetnPrev = mean(OptimRetnPrev),
+            PrEPStopsInd = sum(PrEPStopsInd),
+            PrEPStopsRand = sum(PrEPStopsRand),
+            PrEPStopsRandOptim = sum(PrEPStopsRandOptim)) %>%
   as.data.frame()
-dfr$PSP <- sim$param$prep.start.prob
-dfr$PHA <- sim$param$prep.adhr.dist[3]
-dfr$PDR <- sim$param$prep.discont.rate
+dfr$POIP <- sim$param$prep.optim.init.prob
+dfr$PSPO <- sim$param$prep.start.prob.optim
+dfr$POAC <- sim$param$prep.optim.adhr.cap
+dfr$PADO <- sim$param$prep.adhr.dist.optim[3]
+dfr$PORC <- sim$param$prep.optim.retn.cap
+dfr$PDRO <- sim$param$prep.discont.rate.optim
 dfr$infAvert <- NA
-dfr$scenario <- "n1000"
-dfr <- select(dfr, scenario, PSP, PHA, PDR, everything())
+dfr$scenario <- 500
+dfr <- select(dfr, scenario, everything())
 dfr
 
-dfr.tot <- data.frame(scenario = dfr$scenario[1], PSP = dfr$PSP[1], PHA = dfr$PHA[1], PDR = dfr$PDR[1],
-                      incid = sum(dfr$incid), s.num = sum(dfr$s.num),
-                      prepCurr = sum(dfr$prepCurr), prepCurrHA = sum(dfr$prepCurrHA),
-                      prepRandSt = sum(dfr$prepRandSt), infAvert = NA)
-dfr.tot
-
-
-fn <- list.files("data/", pattern = "sim", full.names = TRUE)
-# fn <- fn[2]
+fn <- list.files("data/", pattern = "sim.n1[0-4][0-9][0-9].rda", full.names = TRUE)
 
 
 # create yearly dataset
 
 doParallel::registerDoParallel(parallel::detectCores())
+doParallel::registerDoParallel(32)
 tdf <- foreach(i = 1:length(fn)) %dopar% {
   counter_df_byyear(fn[i], dfr)
 }
 
+for (i in 1:length(fn)) {
+  df <- counter_df_byyear(fn[i], dfr)
+  if (i == 1) {
+    tdf <- df
+  } else {
+    tdf <- rbind(tdf, df)
+  }
+  cat("\n File ", fn[i], " complete ...")
+}
+
+
 tdf <- do.call("rbind", tdf)
 dim(tdf)
-tdf <- select(tdf, scenario, PSP, PHA, PDR, everything())
 head(tdf)
 
 all <- rbind(dfr, tdf)
@@ -142,24 +216,24 @@ head(all, 30)
 
 all$PinfAvert <- all$infAvert/all$incid
 
-saveRDS(all, file = "data/hold/prepOptim-Yearly-v1.rda", compress = "xz")
+saveRDS(all, file = "data/prepOptim-Yearly-v1.rds", compress = "xz")
 
 
-# create overall dataset
-
-doParallel::registerDoParallel(parallel::detectCores())
-tdf <- foreach(i = 1:length(fn)) %dopar% {
-  counter_df_overall(fn[i], dfr.tot)
-}
-
-tdf <- do.call("rbind", tdf)
-dim(tdf)
-tdf <- select(tdf, scenario, PSP, PHA, PDR, everything())
-head(tdf)
-
-all <- rbind(dfr.tot, tdf)
-head(all, 30)
-
-all$PinfAvert <- all$infAvert/all$incid
-
-saveRDS(all, file = "data/hold/prepOptim-Overall-v1.rda", compress = "xz")
+# # create overall dataset
+#
+# doParallel::registerDoParallel(parallel::detectCores())
+# tdf <- foreach(i = 1:length(fn)) %dopar% {
+#   counter_df_overall(fn[i], dfr.tot)
+# }
+#
+# tdf <- do.call("rbind", tdf)
+# dim(tdf)
+# tdf <- select(tdf, scenario, PSP, PHA, PDR, everything())
+# head(tdf)
+#
+# all <- rbind(dfr.tot, tdf)
+# head(all, 30)
+#
+# all$PinfAvert <- all$infAvert/all$incid
+#
+# saveRDS(all, file = "data/hold/prepOptim-Overall-v1.rda", compress = "xz")
